8ad7d69466c65b4377d272ec4c3f0457cb5a7d9b
a-haber <98184411+a-haber@users.noreply.github.com>
2023-03-07 15:30:19 +0000

    Update IDP_main.ino

--- IDP_main/IDP_main.ino
+++ IDP_main/IDP_main.ino
@@ -1,7 +1,7 @@
 #include "IDP_header.h"
 
 // initialise enums
-OverallState overallState = START_SEQUENCE;
+OverallState overallState = IDLE;
 DrivingState drivingState = NOT_MOVING;
 BlockCollectionState blockCollectionState = DISENGAGED;
 BlockColour blockColour = UNKNOWN;
@@ -17,48 +17,47 @@ void setup() {
   
   // set other inputs/outputs
   setLineFollowerPinout();
-  setUltrasonicSensorPinout();
   initialiseAllLEDs();
-  initInfraredSensor();
-  
-  blockCollectionState = DISENGAGED;
+  setServos();
+  //setUltrasonicSensorPinout();
+  //initInfraredSensor();
 
-  /*
-  if button is to be used to start the program, logic could be: with void onButtonPress() as below
-  pinMode(PUSH_BUTTON_SWITCH, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON_SWITCH), onButtonPress, HIGH);*/
+  // set up push button which will start the robot in a controlled manner
+  pinMode(PUSH_BUTTON_SWITCH, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PUSH_BUTTON_SWITCH), onButtonPress, CHANGE);
 }
-
-/*void onButtonPress {
-  overallState = START_SEQUENCE // (have enums inc. overallState set to NULL originally so the program starts only when the button is pressed?)
-}*/
 
 void loop() {
   switch (overallState) {
+    case IDLE:
+      // do nothing, wait for start sequence to be triggered by button
+      break;
     case START_SEQUENCE:
       //startSequence();
+
+      // proxy start sequence - temporary while line followers not attached
       Serial.println("start sequence");
-      digitalWrite(AMBER_LED, HIGH);
-      driveForward();
+      digitalWrite(GREEN_LED, HIGH);
       delay(1000);
-      overallState = BLOCK_COLLECTION;
-      digitalWrite(AMBER_LED, LOW);
-      break;
-    case IDLE:
+      digitalWrite(GREEN_LED, LOW);
+      overallState = LINE_FOLLOWING;
+
       break;
     case LINE_FOLLOWING:
+      getLineFollowerValues();
       //followLine();
-      digitalWrite(RED_LED, HIGH);
-      Serial.println("line following");
-      releaseMotors();
-      delay(100);
+      //if (isBlockPresent()) {
+      //  overallState = BLOCK_COLLECTION;
+      //}
+      //else if (blockCollectionState = COLLECTED && deliveryZoneReached()) {
+      //  overallState = BLOCK_PLACEMENT
+      //}
+
       break;
     case BLOCK_COLLECTION:
       //hardcode the robot to align with the block and TRY to return to the line 
-      digitalWrite(GREEN_LED, HIGH);
       Serial.println("block collection");
       collectBlockSequence();
-      digitalWrite(GREEN_LED, LOW);
       break;
     case BLOCK_PLACEMENT:
       placeBlockSequence();
@@ -69,4 +68,8 @@ void loop() {
       // error?
       break;
   }
+}
+
+void onButtonPress() {
+  overallState = START_SEQUENCE;
 }
